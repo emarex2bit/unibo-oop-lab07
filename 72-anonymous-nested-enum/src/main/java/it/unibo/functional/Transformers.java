0475c3d9c78c72733870435185cea627be083bc5
Emanuele Vendemini <emanuele.vendemini@studio.unibo.it>
2025-11-06 14:41:30 +0100

    Finished bullet points 1, 2

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,11 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        final var result = new ArrayList<O>();
+        for (final I input : Objects.requireNonNull(base, "The base iterable cannot be null")) {
+            result.add(transformer.call(input));
+        }
+        return result;
     }
 
     /**
@@ -70,7 +74,11 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        final ArrayList<I> l = new ArrayList<>();
+        for (final Collection<? extends I> collection : Objects.requireNonNull(base, "The base iterable cannot be null")) {
+            l.addAll(collection);
+        }
+        return l;
     }
 
     /**
@@ -87,7 +95,11 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        ArrayList<I> l = new ArrayList<>();
+        for (I i : Objects.requireNonNull(base, "The base iterable cannot be null")) {
+            if(test.call(i)) l.add(i);
+        }
+        return l;
     }
 
     /**
@@ -103,6 +115,10 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        ArrayList<I> l = new ArrayList<>();
+        for (I i : Objects.requireNonNull(base, "The base iterable cannot be null")) {
+            if(!test.call(i)) l.add(i);
+        }
+        return l;
     }
 }
